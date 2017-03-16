c79bb9b3e691551c10c4b8668dfc90a46093a57e
shanhong cheng <shanhong.cheng@utah.edu>
2017-03-16 16:07:05 -0600

    drawerlayout

--- app/src/main/java/in/mvpstarter/sample/ui/main/MainActivity.java
+++ app/src/main/java/in/mvpstarter/sample/ui/main/MainActivity.java
@@ -1,10 +1,16 @@
 package in.mvpstarter.sample.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -39,6 +45,9 @@ public class MainActivity extends BaseActivity implements MainMvpView, PokemonAd
     SwipeRefreshLayout mSwipeRefreshLayout;
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
+    
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +56,17 @@ public class MainActivity extends BaseActivity implements MainMvpView, PokemonAd
         mMainPresenter.attachView(this);
 
         setSupportActionBar(mToolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        // Set up the navigation drawer.
+        mDrawerLayout.setStatusBarBackground(R.color.primary_dark);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
 
         mSwipeRefreshLayout.setProgressBackgroundColorSchemeResource(R.color.primary);
         mSwipeRefreshLayout.setColorSchemeResources(R.color.white);
@@ -59,6 +79,35 @@ public class MainActivity extends BaseActivity implements MainMvpView, PokemonAd
         mErrorView.setErrorListener(this);
 
         mMainPresenter.getPokemon(POKEMON_COUNT);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+
+                            default:
+                                break;
+                        }
+                        // Close the navigation drawer when an item is selected.
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Open the navigation drawer when the home icon is selected from the toolbar.
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
